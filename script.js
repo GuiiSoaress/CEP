8b8025ea666ffda71c9a3104955afd6ffc5efdc7
Guilherme Soares <165791068+GuiiSoaress@users.noreply.github.com>
2024-10-04 09:51:53 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 function limpa_formulario_cep(){
-    document.getElementById("cep").value= "...";
-    document.getElementById("rua").value= "...";
-    document.getElementById("bairro").value= "...";
-    document.getElementById("cidade").value= "...";
-    document.getElementById("uf").value= "...";
+    document.getElementById(cep).value= "...";
+    document.getElementById(rua).value= "...";
+    document.getElementById(bairro).value= "...";
+    document.getElementById(cidade).value= "...";
+    document.getElementById(uf).value= "...";
 }
 
 function meu_callback(conteudo){
     if(!("erro" in conteudo)){
-        document.getElementById("rua").value= (conteudo.logradouro);
-        document.getElementById("bairro").value= (conteudo.bairro);
-        document.getElementById("cidade").value= (conteudo.cidade);
-        document.getElementById("uf").value= (conteudo.uf);
+        document.getElementById("rua").value= conteudo.logradouro;
+        document.getElementById("bairro").value= conteudo.bairro;
+        document.getElementById("cidade").value= conteudo.localidade;
+        document.getElementById("uf").value= conteudo.uf;
     }
     else {
         limpa_formulario_cep();
-        alert("Não Achamo Teu CEP seu Zé Ruela")
+        alert("Não Achamo Teu CEP seu Zé Ruela");
     }
 }
 
-function pesquisacep(valor){
+function pesquisaCEP(valor){
 
     var cep = valor.replace(/\D/g,'');
 
@@ -35,11 +35,11 @@ function pesquisacep(valor){
             document.getElementById("uf").value= "...";
             var script = document.createElement('script');
             script.src = 'https://viacep.com.br/ws/'+ cep +'/json/?callback=meu_callback';
-            document.body.appendChild(script)
+            document.body.appendChild(script);
         }
         else {
             limpa_formulario_cep();
-            alert("Vai toma no seu cu seu bosta, bota um cep que funciona ai o cu de burro")
+            alert("Vai toma no seu cu seu bosta, bota um cep que funciona ai o cu de burro");
         }
     }
         else{
